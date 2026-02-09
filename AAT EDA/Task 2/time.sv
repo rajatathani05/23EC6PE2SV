64406e7411ca68644bac947c5a9c97d61b86ef63
rajatathani05 <rajatathani05@gmail.com>
2026-02-09 19:41:37 +0530

    Update and rename time.sv.txt to time.sv

---
+++ AAT EDA/Task 2/time.sv
@@ -0,0 +1,39 @@
+//------------------------------------------------------------------------------
+// File        : digital clock
+// Author      : Rajat Athani / 1BM24EC417
+// Created     : 2026-02-04
+// Module      : Digital clock
+// Project     : SystemVerilog and Verification (23EC6PE2SV)
+// Faculty     : Prof. Ajaykumar Devarapalli
+//
+// Description : This testbench validates the operation of the digital clock by checking
+//               time counting, rollover conditions, and reset functionality through simulation.
+//------------------------------------------------------------------------------
+
+module time_counter (
+  input  logic clk,
+  input  logic rst,
+  output logic [5:0] sec,
+  output logic [5:0] min
+);
+
+  always_ff @(posedge clk or posedge rst) begin
+    if (rst) begin
+      sec <= 0;
+      min <= 0;
+    end
+    else begin
+      if (sec == 59) begin
+        sec <= 0;
+        if (min == 59)
+          min <= 0;
+        else
+          min <= min + 1;
+      end
+      else begin
+        sec <= sec + 1;
+      end
+    end
+  end
+
+endmodule
